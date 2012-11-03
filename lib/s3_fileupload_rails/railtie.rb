cbe811291faafd1468549b8710337e3573738bb8
allen13 <kex.allen13@gmail.com>
2012-11-03 19:27:35 -0400

    Removed railtie.

--- lib/s3_fileupload_rails/railtie.rb
+++
@@ -1,8 +0,0 @@
-require 's3_fileupload_rails/view_helpers'
-module S3FileuploadRails
-  class Railtie < Rails::Railtie
-    initializer "s3_fileupload_rails.view_helpers" do
-      ActionView::Base.send :include, ViewHelpers
-    end
-  end
-end
