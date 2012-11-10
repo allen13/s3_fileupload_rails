c2da6062dd7a97ab7d5afa2e4e63239a2eb4ee7c
allen13 <kex.allen13@gmail.com>
2012-11-10 11:18:53 -0500

    Cleaned up controller.

--- app/controllers/s3_fileupload_rails/application_controller.rb
+++
@@ -1,4 +0,0 @@
-module S3FileuploadRails
-  class ApplicationController < ActionController::Base
-  end
-end
--- app/controllers/s3_fileupload_rails/s3_upload_controller.rb
+++ app/controllers/s3_fileupload_rails/s3_upload_controller.rb
@@ -1,77 +1,45 @@
-require_dependency "s3_fileupload_rails/application_controller"
-
 module S3FileuploadRails
   class S3UploadController < ApplicationController
-  	def index
-  	  render json: '{"key":"/upload/path"}'
-  	end
 
   	def index
       render json: {
-        policy: policy,
+        key: key,
         signature: signature,
-        key: key
+        policy: policy
       }
     end
 
     private
 
-      # generate the policy document that amazon is expecting.
-      def s3_upload_policy_document
-        Base64.encode64(
-          {
-            expiration: 30.minutes.from_now.utc.strftime('%Y-%m-%dT%H:%M:%S.000Z'),
-            conditions: [
-              { bucket: ENV['AWS_S3_BUCKET'] ||= "" },
-              { acl: 'public-read' },
-              ["starts-with", "$key", "uploads/"],
-              { success_action_status: '201' }
-            ]
-          }.to_json
-        ).gsub(/\n|\r/, '')
+      def key
+        "uploads/#{SecureRandom.uuid}/#{(params[:file] if params)}"
       end
-  
-      # sign our request by Base64 encoding the policy document.
-      def s3_upload_signature
+      
+      def signature
         Base64.encode64(
           OpenSSL::HMAC.digest(
             OpenSSL::Digest::Digest.new('sha1'),
-            ENV['AWS_SECRET_ACCESS_KEY'] ||= "",
-            s3_upload_policy_document
+            ENV['AWS_SECRET_ACCESS_KEY'] ||= "", 
+            policy
           )
-        ).gsub(/\n/, '')
+        ).gsub("\n", "")
       end
 
-    def key
-      "uploads/#{SecureRandom.uuid}/#{(params[:file] if params)}"
-    end
-
-    def policy
-      Base64.encode64(policy_data.to_json).gsub("\n", "")
-    end
-
-    def policy_data
-      {
-        expiration: 10.hours.from_now,
-        conditions: [
-          ["starts-with", "$utf8", ""],
-          ["starts-with", "$key", ""],
-          ["content-length-range", 0, 500.megabytes],
-          {bucket: (ENV['AWS_S3_BUCKET'] ||= "")},
-          {acl: "public-read"}
-        ]
-      }
-    end
-
-    def signature
-      Base64.encode64(
-        OpenSSL::HMAC.digest(
-          OpenSSL::Digest::Digest.new('sha1'),
-          ENV['AWS_SECRET_ACCESS_KEY'] ||= "", 
-          policy
-        )
-      ).gsub("\n", "")
-    end
-
+      def policy
+        Base64.encode64(policy_data.to_json).gsub("\n", "")
+      end
+  
+      def policy_data
+        {
+          expiration: 10.hours.from_now,
+          conditions: [
+            ["starts-with", "$utf8", ""],
+            ["starts-with", "$key", ""],
+            ["content-length-range", 0, 500.megabytes],
+            {bucket: (ENV['AWS_S3_BUCKET'] ||= "")},
+            {acl: "public-read"}
+          ]
+        }
+      end
   end
 end
